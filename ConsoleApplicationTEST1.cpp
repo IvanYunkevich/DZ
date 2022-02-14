75b55f29913bc4b14f7fc0289649276f4c0ed220
IvanYunkevich <ivan01089@mail.ru>
2022-02-14 15:00:41 +0300

    homework5

---
+++ ConsoleApplicationTEST1.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplicationTEST1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+void PrintArray(const int size, int* arr)
+{
+
+    for (int i = 0, j = 1; i < size; j += 3, ++i)
+    {
+        arr[i] = j;
+        cout << j << " ";
+    }
+}
+int main() 
+{
+    const int SIZE = 8;
+    int arr[SIZE];
+    PrintArray(SIZE, arr);
+    return 0;
+}
